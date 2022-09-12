ad5a7edf0f53eda00e6ed137796b6f5cd4417173
Dunmininu Oduwole <Dunmininu Oduwole>
2022-09-12 05:21:15 -0700

    Puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,18 +1,23 @@
-#include <stdio.h>
-/**
+#include <stdio.h>    
+/**                                                                                                                                                             
 
- * main - prints the phrase "Programming is like building a multilingual puzzle
+* main - print the string in the put function                                                                                                                    
 
- * Return: 0 if exited properly, non-zero otherwise
+*                                                                                                                                                               
 
- */
+* Description: using the main function                                                                                                                          
 
-int main(void)
+* this program prints "Programming is like building a multilingual puzzle                                                                                      
 
-{
+* Return: 0                                                                                                                                                     
 
-	puts("\"Programming is like building a multilingual puzzle");
+*/                                                                                                                                                              
 
-	return (0);
+int main{void}                                                                                                                                                  
 
-}
+{                                                                                                                                                               
+
+        puts("\"Programming is like building a multilingual puzzle");                                                                                           
+
+        return (0);                                                                                                                                             
+
